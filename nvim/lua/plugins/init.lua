fa1a5a90aa8012004c888067925b65c598b632d0
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2026-01-03 00:08:33 -0500

    feat: added nvim, kitty, sketchybar

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,10 @@
+require("lazy").setup({
+  require("plugins.gruvbox"),
+  require("plugins.devicons"),
+  require("plugins.nonicons"),
+  require("plugins.lualine"),
+  require("plugins.alpha"),
+  require("plugins.wakatime"),
+  require("plugins.noice"),
+})
+

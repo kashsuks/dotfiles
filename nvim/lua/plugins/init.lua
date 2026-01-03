9bed36dceb5dd06a647ce59d71ba7e1b37e6c678
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2026-01-03 12:28:12 -0500

    feat: using nvchad instead

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,11 +1,28 @@
-require("lazy").setup({
-  require("plugins.dracula"),
-  require("plugins.devicons"),
-  require("plugins.nonicons"),
-  require("plugins.lualine"),
-  require("plugins.alpha"),
-  require("plugins.wakatime"),
-  require("plugins.noice"),
-  require("plugins.snacks"),
-})
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
 
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfigw",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
--- nvim/lua/plugins/nonicons.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "yamatsum/nvim-nonicons",
-  config = function()
-    require("nvim-nonicons").setup({
-      override = {},
-    })
-  end,
-}
-

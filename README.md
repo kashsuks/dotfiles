b8049bc3e58527ca613c18a34321d11c46aef67c
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2026-01-30 12:21:00 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dotfiles
-Mac Rice dotfiles
+
+These are my mac dotfiles that I use on the daily. I went for a dark blueberry theme and I would say it worked out pretty well!
+
+![image1](blob:https://imgur.com/49745d1f-ab1d-4e7b-ac8a-704f394f2c3d)
+![image2](blob:https://imgur.com/53d4e2b4-a5bc-4140-b6ff-bc791ccdf15c)
+![image3](blob:https://imgur.com/31d4af3c-70da-4235-a55f-3cb0873c5ce0)
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,6 +1,22 @@
 require "nvchad.options"
 
--- add yours here!
+-- ================================
+-- Your custom options
+-- ================================
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+-- Show cursor line highlight on both number + text
+vim.o.cursorlineopt = "both"
+
+-- --------------------------------
+-- Line numbers (NvChad-safe)
+-- --------------------------------
+-- number & relativenumber are WINDOW-LOCAL options.
+-- NvChad/plugins may reset them when windows are created,
+-- so we enforce them on window enter.
+
+vim.api.nvim_create_autocmd({ "BufWinEnter", "WinEnter" }, {
+  callback = function()
+    vim.opt_local.number = true
+    vim.opt_local.relativenumber = true
+  end,
+})
